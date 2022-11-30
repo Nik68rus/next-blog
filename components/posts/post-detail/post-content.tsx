3fea5053fc7cf7ca09a9a810578ec801a5976585
Nik68rus <nik200x@mail.ru>
2022-11-30 18:05:30 +0300

    optimization

--- components/posts/post-detail/post-content.tsx
+++ components/posts/post-detail/post-content.tsx
@@ -4,8 +4,13 @@ import ReactMarkdown from 'react-markdown';
 import { IPost } from '../../../types';
 import classes from './post-content.module.css';
 import PostHeader from './post-header';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
+import atomDark from 'react-syntax-highlighter/dist/cjs/styles/prism/atom-dark';
+import js from 'react-syntax-highlighter/dist/cjs/languages/prism/javascript';
+import css from 'react-syntax-highlighter/dist/cjs/languages/prism/css';
+
+SyntaxHighlighter.registerLanguage('js', js);
+SyntaxHighlighter.registerLanguage('css', css);
 
 type Props = {
   post: IPost;
